aa76d98f701f6c34475da3d1421f17bb53ab1f05
taylorchu <tailinchu@gmail.com>
2015-04-18 18:39:59 -0700

    move segmented fetcher

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -1,38 +1,13 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
 	"fmt"
 	"net"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/go-ndn/mux"
 	"github.com/go-ndn/ndn"
 )
-
-func segmentFetcher(face *ndn.Face, s string) []byte {
-	var content []byte
-	var start int
-	segNum := make([]byte, 8)
-	base := ndn.NewName(s)
-	for {
-		binary.BigEndian.PutUint64(segNum, uint64(start))
-		d, ok := <-face.SendInterest(&ndn.Interest{
-			Name: ndn.Name{Components: append(base.Components, segNum)},
-		})
-		if !ok {
-			return nil
-		}
-		content = append(content, d.Content...)
-		if len(d.Name.Components) > 0 &&
-			!bytes.Equal(d.Name.Components[len(d.Name.Components)-1], d.MetaInfo.FinalBlockID.Component) {
-			start += len(d.Content)
-		} else {
-			break
-		}
-	}
-	return content
-}
 
 func main() {
 	conn, err := net.Dial("tcp", ":6363")
@@ -43,5 +18,5 @@ func main() {
 	face := ndn.NewFace(conn, nil)
 	defer face.Close()
 
-	spew.Dump(segmentFetcher(face, "/hello"))
+	spew.Dump(mux.Assemble(face, ndn.NewName("/hello")))
 }
